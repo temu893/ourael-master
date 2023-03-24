d8f84d301b075f94a6810876c68c2225dd4cf404
temesgen <65918861+temu893@users.noreply.github.com>
2023-03-23 19:10:17 -0500

    Add files via upload

---
+++ js/langscripts.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let langs = document.querySelector(".langs"),
+      link = document.querySelectorAll(".lang-link"),
+      translatableElements = document.querySelectorAll("[data-lang-key]");
+  
+    link.forEach((el) => {
+      el.addEventListener("click", () => {
+        langs.querySelector(".active").classList.remove("active");
+        el.classList.add("active");
+  
+        let attr = el.getAttribute("language");
+        updateTranslations(attr);
+      });
+    });
+  
+    function updateTranslations(lang) {
+      translatableElements.forEach((element) => {
+        const key = element.getAttribute("data-lang-key");
+        element.textContent = data[lang][key];
+      });
+    }
+  
+    let data = {
+        amharic: {
+          // ... existing Amharic translations ...
+          "nav-home": "ቀዳሚ ገጽ",
+          "nav-about": "ማንነታችን",
+          "nav-events": "ክስተቶች",
+          "nav-contact": "ግንኙነት",
+          "hero-title": "እኛ የምንኖርበት እግዚአብሔርን አክብርና ደቀ መዛሙርት እናፍቅር",
+          "hero-social-title": "ይከታተሉን"
+
+        },
+        english: {
+          // ... existing English translations ...
+          "nav-home": "Home",
+          "nav-about": "About",
+          "nav-events": "Events",
+          "nav-contact": "Contact",
+          "hero-title": "We Exist To Honor God And Make Disciples",
+          "hero-social-title": "Follow Us"
+
+        },
+      };
+    
+  });
+  
+// ... existing code ...
+
+
+// ... existing code ...
